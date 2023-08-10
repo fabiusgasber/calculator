d28019a36a33828c6806a5fb7735a831f9e03277
Fabius Gasber <fabiusgasber@gmail.com>
2023-08-10 16:36:25 +0200

    Add function to display values

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const buttons = document.querySelectorAll('button');
+const display = document.querySelector('.calc-display');
+let displayValue;
+
 function calculate(number1, number2, operator) {
     let a = getNumber(number1)
     let b = getNumber(number2);
@@ -23,3 +27,14 @@ function getNumber(a){
     return parseInt(a);
   }
 }
+
+function populateDisplay(){
+    buttons.forEach((button) => {
+        
+        button.addEventListener('click', () => {
+            displayValue = button.textContent;
+            display.textContent = button.textContent;
+        });
+    });
+}
+populateDisplay();
